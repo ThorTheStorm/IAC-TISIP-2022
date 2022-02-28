a3a85e998d4cd9201bbc7e1e034da22ccbe249be
Tor Egil Grinnen Berntsen <tor.egil@tbhome.no>
2022-02-28 22:30:27 +0100

    Configured JSON-spec for storage account and rollout script in powershell

--- Modul-03.ps1
+++ Modul-03.ps1
@@ -1,13 +1,14 @@
 $TenantID = '02feabb9-444e-4f66-9c13-6a8f04b75c2f'
 $SubscriptionID = 'efc1e7b1-5729-4eea-b33e-12cc6b1c0183'
-$Credentials = Get-Credential
+$Credentials = Get-Credential -Prompt
 
 Connect-AzAccount -ServicePrincipal -Credential $Credentials -TenantID $TenantID -Subscription $SubscriptionID
 
 $ResourceGroup = 'Modul-03'
+$ResourceDirectory = 'C:\IAC-TISIP-2022'
 
-New-AzResourceGroup -Name $ResourceGroup -Location 'West-Europe' -Force
+New-AzResourceGroup -Name $ResourceGroup -Location 'West Europe' -Force
 New-AzResourceGroupDeployment `
-    -Name 'Modul-03-Storage' `
+    -Name 'Modul-03-Storage-TB' `
     -ResourceGroupName $ResourceGroup `
-    -TemplateFile '.\Modul-03.json'
+    -TemplateFile "$ResourceDirectory\Modul-03.json"
